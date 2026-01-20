035a5286d1925680e91b1300e6a5f53ab4e0b6d6
github <pgf1mckazu3@gmail.com>
2026-01-20 22:34:27 +0900

    SelfIntroducの出力

---
+++ HelloWorld_Kazuya/src/sample2/Sample2.java
@@ -0,0 +1,24 @@
+package sample2;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// mainメソッドの処理内容
+		//氏名
+		String name = "山田 花子";
+		//年齢
+		int age = 25;
+		//趣味
+		String hobby =  "映画鑑賞";
+		
+		/*
+		 *  自己紹介文を出力する処理
+		 */
+		
+		System.out.println("わたなしの名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		
+	}
+
+}
